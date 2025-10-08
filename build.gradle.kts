9fbd3c4af7e5fea2d0fb0bc7f9cffcb6fba56672
mincHR549 <152802566+mincHR549@users.noreply.github.com>
2025-10-08 09:26:15 +0800

    更新tb

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
 
             install(BukkitNMSDataSerializer)
         }
-        version { taboolib = "6.2.3-2eb93b5" }
+        version { taboolib = "6.2.3-1a8d7125" }
     }
     repositories {
         mavenLocal()
